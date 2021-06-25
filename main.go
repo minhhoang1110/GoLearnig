8f153b0de8e9f0c239447e29fef29c1a1fb17f46
Truong Minh Hoang <minhhoang111099@gmail.com>
2021-06-25 15:27:39 +0700

    routine and chanel

--- main.go
+++ main.go
@@ -1,28 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-type Person interface {
-	getInfomation() string
-}
-type Student struct {
-	name string
-	age  int
-}
-type Teacher struct {
-	name string
-	age  int
-}
-
-func (student Student) getInfomation() string {
-	return "I'm " + student.name + " ," + fmt.Sprint(student.age) + " year old. I'm a student."
-}
-func (teacher Teacher) getInfomation() string {
-	return "I'm " + teacher.name + " ," + fmt.Sprint(teacher.age) + " year old. I'm a teacher."
+func checkResStatus(chanel chan string, link string) {
+	res, err := http.Get(link)
+	if err != nil {
+		chanel <- fmt.Sprintf("Request %s is failed, status: %s, prototype: %s, server name: %s.", link, res.Status, res.Proto, res.TLS.ServerName)
+		return
+	}
+	chanel <- fmt.Sprintf("Request %s is success, status: %s, prototype: %s, server name: %s.", link, res.Status, res.Proto, res.TLS.ServerName)
 }
 func main() {
-	student := Student{"Ben", 12}
-	teacher := Teacher{"Henry", 35}
-	fmt.Println(student.getInfomation())
-	fmt.Println(teacher.getInfomation())
+	listSite := []string{"https://www.youtube.com", "https://www.facebook.com"}
+	chanel := make(chan string)
+	for _, site := range listSite {
+		go checkResStatus(chanel, site)
+	}
+	responses := map[int]string{}
+	for index, _ := range listSite {
+		responses[index] = <-chanel
+	}
+	for _, res := range responses {
+		fmt.Println(res)
+	}
 }
